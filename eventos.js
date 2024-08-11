281d39a8743f22c9f5de2264ea3ee5b75deb167f
mateodlso <130793150+mateodlso@users.noreply.github.com>
2024-08-11 20:49:25 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     const boton = document.getElementById("boton");
-    boton.onclick = function() {
+    boton.addEventListener("click", function(event) {
         alert("Hola!");
-    };
+        event.stopPropagation(); // Evitando que el evento se propague al div
+    });
 
     const contenedor = document.getElementById("contenedor");
     contenedor.addEventListener("click", function() {
