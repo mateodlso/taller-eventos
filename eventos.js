180d808d02f45d19a56aad6daf6bf74099b0045e
mateodlso <130793150+mateodlso@users.noreply.github.com>
2024-08-11 20:23:00 -0300

    Asociacion de archivo eventos.js

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const boton = document.getElementById("boton");
+    boton.onclick = function() {
+        alert("Hola!");
+    };
+});
